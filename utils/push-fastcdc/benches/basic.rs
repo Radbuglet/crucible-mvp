a272baf046f066e0325973a338174c97e417a99e
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2025-08-29 13:35:32 -0600

    benchmark `push-fastcdc`

---
+++ utils/push-fastcdc/benches/basic.rs
@@ -0,0 +1,42 @@
+use std::fs;
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use fastcdc::v2020::FastCDC;
+use push_fastcdc::{GearConfig, GearState, GearTablesRef};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let read_result = fs::read("test/SekienAkashita.jpg").unwrap();
+
+    c.bench_function("reference", |b| {
+        b.iter(|| {
+            let cdc = FastCDC::new(&read_result, 4096, 16384, 65535);
+
+            for _ in cdc {}
+        });
+    });
+
+    c.bench_function("push", |b| {
+        b.iter(|| {
+            let config = GearConfig::new(4096, 16384, 65535);
+            let tables = GearTablesRef::new();
+
+            let mut state = GearState::new();
+
+            let mut remaining = read_result.as_slice();
+
+            while !remaining.is_empty() {
+                match state.push(&config, tables, remaining) {
+                    Some((read, GearState { .. })) => {
+                        remaining = &remaining[read..];
+                    }
+                    None => {
+                        break;
+                    }
+                }
+            }
+        });
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
